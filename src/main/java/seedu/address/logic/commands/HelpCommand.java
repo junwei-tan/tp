0466dc9797940475485b7af12745f985b728d04a
junwei-tan <junweitan@u.nus.edu>
2022-10-27 18:41:49 +0800

    Resolve merge conflict

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -1,14 +1,8 @@
 package seedu.address.logic.commands;
 
-<<<<<<< HEAD
 import java.util.stream.Collectors;
 
 import seedu.address.commons.core.Commands;
-=======
-import seedu.address.logic.commands.contact.AddContactCommand;
-import seedu.address.logic.commands.tag.AddTagCommand;
-import seedu.address.logic.commands.task.AddTaskCommand;
->>>>>>> master
 import seedu.address.model.Model;
 
 /**
@@ -26,19 +20,10 @@ public class HelpCommand extends Command {
     private static final String USER_GUIDE_URL =
             "https://ay2223s1-cs2103t-f11-4.github.io/tp/UserGuide.html#quick-start";
 
-<<<<<<< HEAD
     public static final String SHOWING_HELP_MESSAGE = "User Guide: "
             + USER_GUIDE_URL + "\n\n"
             + "Sample Commands: \n"
             + Commands.getAllCommands().stream().map(command -> command + "\n").collect(Collectors.joining());
-=======
-    public static final String SHOWING_HELP_MESSAGE = "Sample usage:\n\n"
-            + AddContactCommand.MESSAGE_USAGE + "\n\n"
-            + AddTaskCommand.MESSAGE_USAGE + "\n\n"
-            + AddTagCommand.MESSAGE_USAGE + "\n\n"
-            + "For other commands and further help:\n"
-            + USER_GUIDE_URL;
->>>>>>> master
 
     @Override
     public CommandResult execute(Model model) {
